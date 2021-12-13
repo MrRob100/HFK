cb12deba4e62ee4a536214f4612c74ecea7db805
horidhenry <robarnold100@hotmail.com>
2021-12-13 12:54:34 -0300

    removed route

--- routes/web.php
+++ routes/web.php
@@ -18,10 +18,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 /* chart data */
 Route::get('/chart_data', [ChartController::class, 'data'])->name('chart.data');
 
